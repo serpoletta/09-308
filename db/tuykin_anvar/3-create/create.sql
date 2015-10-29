754c0ca0f07b456531318877c15cf99a00dfb2ee
Anvar Tuykin <tuykin@gmail.com>
2015-10-29 12:00:09 +0300

    Add tables and some data

--- db/tuykin_anvar/3-create/create.sql
+++ db/tuykin_anvar/3-create/create.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE university;
 
+DROP TABLE IF EXISTS lessons;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS professors;
+DROP TABLE IF EXISTS subjects;
 
 CREATE TABLE groups (
 	id serial PRIMARY KEY,
@@ -15,4 +18,25 @@ CREATE TABLE students (
 	surname VARCHAR NOT NULL,
 	birthday DATE,
 	group_id INTEGER REFERENCES groups(id)
+);
+
+CREATE TABLE professors (
+	id serial PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	surname VARCHAR NOT NULL,
+	birthday DATE
+);
+
+CREATE TABLE subjects (
+	id serial PRIMARY KEY,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE lessons (
+	id serial PRIMARY KEY,
+	professor_id INTEGER REFERENCES professors(id),
+	group_id INTEGER REFERENCES groups(id),
+	subject_id INTEGER REFERENCES subjects(id)
+	day INTEGER NOT NULL,
+	time TIME NOT NULL
 );
