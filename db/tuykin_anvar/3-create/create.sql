98863c69844b1235c75993be06fb1cd3bcdc2e8e
Anvar Tuykin <tuykin@gmail.com>
2015-10-29 15:56:49 +0300

    Update create.sql

--- db/tuykin_anvar/3-create/create.sql
+++ db/tuykin_anvar/3-create/create.sql
@@ -1,43 +1,61 @@
-CREATE DATABASE university;
-
 DROP TABLE IF EXISTS lessons;
+DROP TABLE IF EXISTS lessons_time;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS professors;
 DROP TABLE IF EXISTS subjects;
 
+DROP TYPE TGENDER;
+DROP TYPE TWEEKDAY;
+
+CREATE TYPE TGENDER AS ENUM ('м', 'ж');
+CREATE TYPE TWEEKDAY AS ENUM ('ПН','ВТ','СР','ЧТ','ПТ','СБ','ВС');
+
 CREATE TABLE groups (
-	id serial PRIMARY KEY,
-	name VARCHAR NOT NULL,
-	entry_year SMALLINT NOT NULL
+    id serial PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    entry_year SMALLINT NOT NULL CHECK (entry_year BETWEEN 1800 AND 2100)
 );
 
 CREATE TABLE students (
-	id serial PRIMARY KEY,
-	name VARCHAR NOT NULL,
-	surname VARCHAR NOT NULL,
-	birthday DATE,
-	phone VARCHAR(10) UNIQUE,
-	group_id INTEGER REFERENCES groups(id)
+    id serial PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    middlename VARCHAR(50),
+    birthday DATE,
+    gender TGENDER NOT NULL,
+    phone VARCHAR(10) UNIQUE,
+    group_id INTEGER REFERENCES groups(id)
 );
 
 CREATE TABLE professors (
-	id serial PRIMARY KEY,
-	name VARCHAR NOT NULL,
-	surname VARCHAR NOT NULL,
-	birthday DATE
+    id serial PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    middlename VARCHAR(50),
+    birthday DATE,
+    gender TGENDER NOT NULL
 );
 
 CREATE TABLE subjects (
-	id serial PRIMARY KEY,
-	title VARCHAR NOT NULL
+    id serial PRIMARY KEY,
+    title VARCHAR NOT NULL
+);
+
+CREATE TABLE lessons_time (
+    id serial PRIMARY KEY,
+    starts_at TIME NOT NULL,
+    ends_at TIME NOT NULL,
+    ordering SMALLINT
 );
 
 CREATE TABLE lessons (
-	id serial PRIMARY KEY,
-	professor_id INTEGER REFERENCES professors(id),
-	group_id INTEGER REFERENCES groups(id),
-	subject_id INTEGER REFERENCES subjects(id)
-	day INTEGER NOT NULL,
-	time TIME NOT NULL
+    id serial PRIMARY KEY,
+    professor_id INTEGER REFERENCES professors(id),
+    group_id INTEGER REFERENCES groups(id),
+    subject_id INTEGER REFERENCES subjects(id),
+    weekday TWEEKDAY NOT NULL,
+    -- week_parity: четность недели; 1: каждую неделю, 2: четные недели, 3: нечетные недели
+    week_parity SMALLINT NOT NULL CHECK (week_parity BETWEEN 1 AND 3),
+    lesson_time INTEGER REFERENCES lessons_time
 );
